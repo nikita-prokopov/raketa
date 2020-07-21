4f932d2f0e9584371bcf4f716be508c920cfde69
nikita-prokopov <nikita-prokopov@bk.ru>
2020-07-21 18:59:16 +0300

    Updates

---
+++ precache-manifest.81cb4cd06fd099b6d5d57579ef579858.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32303f02d7a79e0a0faf011d3381157c",
+    "url": "/raketa/index.html"
+  },
+  {
+    "revision": "d2fac32b23d8851fae02",
+    "url": "/raketa/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a066521902e993de90b4",
+    "url": "/raketa/static/css/main.85a92180.chunk.css"
+  },
+  {
+    "revision": "d2fac32b23d8851fae02",
+    "url": "/raketa/static/js/2.9b33a83a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/raketa/static/js/2.9b33a83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a066521902e993de90b4",
+    "url": "/raketa/static/js/main.5f57a0a4.chunk.js"
+  },
+  {
+    "revision": "1a669b1295146b06364e",
+    "url": "/raketa/static/js/runtime-main.d94387e8.js"
+  },
+  {
+    "revision": "2b90ad699c5a853674ec2d2b543be274",
+    "url": "/raketa/static/media/loupe.2b90ad69.svg"
+  }
+]);
